fedb6abb0c2a02fc89dc846d38a0d35b3405e4bc
yavbatyr <95848367+yavbatyr@users.noreply.github.com>
2022-08-17 15:29:06 +0300

    created 'footer' and animated popup menu on click a burger

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,13 @@
 $(function(){
+    $('.header__burger').on('click', function(){
+        $('.header__menu-list').toggleClass('header__menu-list--active');
+
+        $(document).on('click', function(e) {
+        if (!$(e.target).closest(".header__burger").length) {
+            $('.header__menu-list').toggleClass('header__menu-list--active');
+        }
+        e.stopPropagation();
+        });
+    })
 
 })
